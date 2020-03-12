9e08c3d516418ae69b0af631722322f282a8e930
strrugleforlife <kuro.y6222@gmail.com>
2020-03-12 16:07:11 +0900

    test

--- src/main/java/jp/co/strrugleforlife/MainServlet.java
+++ src/main/java/jp/co/strrugleforlife/MainServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,10 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MainServlet
  */
-@WebServlet(
-        name = "MainServlet",
-        urlPatterns = {"/MainServlet"}
-    )
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
